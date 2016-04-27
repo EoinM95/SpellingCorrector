fbb787eec8e9e249d5c75d3bfa5f5f430cafaf65
Eoin Murphy <murphe43@tcd.ie>
2016-04-27 20:15:18 +0100

    Levenstein distance metric added

--- Corrector.java
+++ Corrector.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 
 public class Corrector {
 	
-	public static final int MAX_EDIT_DISTANCE=2;
+	public static final int MAX_EDIT_DISTANCE=4;
 	
 	public static FrequencyModel trainModel(){
 		ArrayList<String> lines=FileIO.readFromFile("Lexique.txt");
@@ -21,7 +22,28 @@ public class Corrector {
 	}
 	
 	public static String correct(String word,FrequencyModel model){
-		ArrayList<Set<String>> corrections = CorrectionGenerator.generateCorrections(word);
+		ArrayList<Set<String>> corrections;
+		int wordLength=word.length();
+		int generationCost=wordLength*2+wordLength-1+CorrectionGenerator.ALPHABET.length*wordLength;
+		generationCost=(int)(Math.pow(generationCost,MAX_EDIT_DISTANCE));
+		if(generationCost<model.keySet().size()*wordLength){
+			corrections = CorrectionGenerator.generateCorrections(word);
+		}	
+		else{
+			corrections=new ArrayList<Set<String>>();
+			HashSet<String> singleWord=new HashSet<String>();
+			singleWord.add(word);
+			corrections.add(singleWord);
+			for(int i=1;i<=MAX_EDIT_DISTANCE;i++){
+				corrections.add(new HashSet<String>());
+			}
+			for(String lexWord:model.keySet()){
+				int editDistance=Distance.levensteinDistance(word,lexWord);
+				if(editDistance>0&&editDistance<=MAX_EDIT_DISTANCE){
+					corrections.get(editDistance).add(lexWord);
+				}
+			}
+		}
 		for(Set<String> editSet:corrections){
 			editSet.retainAll(model.keySet());
 			if(!editSet.isEmpty())
